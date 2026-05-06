3ba37b75bca3dcda8617f08d854fd426b9477c72
Devanand-PA <devanandpa721@gmail.com>
2026-05-06 21:54:10 +0530

    added custom autocomplete to nvim

--- XDG_CONFIG/nvim/init.lua
+++ XDG_CONFIG/nvim/init.lua
@@ -1,9 +1,10 @@
--- [NVIM INIT.LUA FILE] --
+--- [NVIM INIT.LUA FILE] --
 
 -------------------------
--- [Default Options] ---
+--- [Default Options] ---
 vim.o.mouse = "" -- disable mouse
 vim.opt.ignorecase = true -- case insensitive search
+vim.opt.title = true
 vim.opt.titlestring = "%{$PWD}$ %f" -- title
 vim.opt.clipboard = "unnamedplus"
 vim.opt.spell = true -- enable spellcheck
@@ -13,7 +14,8 @@ homedir = os.getenv("HOME")
 -------------------------
 
 -------------------------
--- [Visual Aids] --------
+--- [Visual Aids] --------
+--- vim.opt.number = true
 vim.opt.list = true -- symbols displayed during linewraps, tab, etc
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.showbreak = "↪ "
@@ -21,12 +23,13 @@ vim.opt.inccommand = "split" -- show the pattern being searched in a separate sp
 vim.opt.cursorline = true	-- highlight the line where a search result is
 vim.opt.hlsearch = true		-- highlight all matches to a search
 vim.opt.signcolumn = "yes"      -- Reserves space for symbols, preventing text from shifting
+
 -------------------------
 -------------------------
 
 
 -------------------------
--- [Split Behaviour] ----
+--- [Split Behaviour] ----
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 -------------------------
@@ -34,7 +37,7 @@ vim.opt.splitbelow = true
 
 
 -------------------------
--- [Keymaps] ------------
+--- [Keymaps] ------------
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- remove search highlights
@@ -59,5 +62,39 @@ keymap("n", "w<Right>", "<C-w><Right>", opts)
 keymap("n", "w<Up>", "<C-w><Up>", opts)
 keymap("n", "w<Down>", "<C-w><Down>", opts)
 
+-------------------------
+-------------------------
+
+-------------------------
+-----[Autocomplete] -----
+-- Completion sources (current buffer + other buffers)
+vim.opt.complete = { ".", "b" }
+
+-- Make popup behave like autocomplete
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+
+-- Auto-trigger completion while typing
+vim.api.nvim_create_autocmd("TextChangedI", {
+  callback = function()
+    -- Don't trigger inside completion already
+    if vim.fn.pumvisible() == 0 then
+      -- Get current line and cursor position
+      local col = vim.fn.col(".") - 1
+      if col > 0 then
+        local line = vim.fn.getline(".")
+        local char_before = line:sub(col, col)
+
+        -- Trigger only if previous character is part of a word
+        if char_before:match("%w") then
+          vim.api.nvim_feedkeys(
+            vim.api.nvim_replace_termcodes("<C-n>", true, false, true),
+            "n",
+            true
+          )
+        end
+      end
+    end
+  end,
+})
 -------------------------
 -------------------------
